44ac840a5ea56e38e9c6161841e0a94acae86956
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-17 11:13:02 -0800

    no hello in main

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import(
 "fmt"
-"github.com/khurlbut/trader/hello"
 "github.com/khurlbut/trader/prototype"
 )
 
 func main() {
-	fmt.Println(hello.BuildHello())
-	fmt.Println(hello.BuildHi())
 	fmt.Println("hey", prototype.PricingLoop())
 }
