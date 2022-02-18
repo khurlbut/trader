5d4db03ecc1d63b3a022a0a8fa5663121b5e81e1
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-18 12:37:51 -0800

    coding

--- price_quotes/log_based_quotes/cryptodatadownload/price_quotes/quote.go
+++ price_quotes/log_based_quotes/cryptodatadownload/price_quotes/quote.go
@@ -12,6 +12,10 @@ import (
 
 const datafile = "/Users/Ke015t7/.gvm/pkgsets/go1.17.7/global/src/github.com/khurlbut/trader/data/cryptodatadownload/Binance_BTCUSDT_minute.csv"
 const open_index = 3
+const date_time_index = 1
+const date_time_layout = "2006-01-02 15:04:05"
+const start_time = time.Parse(date_time_layout, "2020-11-21 07:21:00")
+const end_time = time.Parse(date_time_layout, "2020-12-21 07:21:00")
 
 var file *os.File = nil
 var scanner *bufio.Scanner = nil
@@ -24,7 +28,45 @@ func Init() {
      }
      file = f
      scanner = bufio.NewScanner(file)
+     checkScanner()
+}
 
+func scanToStartDate() {
+     // line := readLine()
+     
+     // t, err := time.Parse(layout, line_arr[date_time_index])
+     // if err != nil {
+     //      log.Fatal(err)
+     // }
+
+     for readDate().Before(start_time) {
+         scanner.Scan() 
+     }
+}
+
+func readPrice() float64 {
+     p, err := strconv.ParseFloat(readLineArray()[open_index], 64) 
+     if err != nil {
+          log.Fatal(err)
+     }
+     return p
+}
+
+func readDate() time.Time {
+     t, err := time.Parse(layout, readLineArray()[date_time_index])
+     if err != nil {
+          log.Fatal(err)
+     }
+     return t
+}
+
+func readLineArray() []string {
+     l := readLine()
+     return strings.Split(line, ",")
+}
+
+func readLine() string {
+     line := scanner.Text()
      checkScanner()
 }
 
@@ -39,33 +81,37 @@ func Close() {
      file.Close()
 }
 
-var i = 0
+// var i = 0
 func HasNextPrice() bool {
-     if i < 100 {
-          i++
-          scanner.Scan()
-          checkScanner()
-          return true
-     }
-     return false
+     return readDate().Before(end_time)
+     // if i < 100 {
+     //      i++
+     //      scanner.Scan()
+     //      checkScanner()
+     //      return true
+     // }
+     // return false
 }
 
 func NextPrice() float64 {
-     line := scanner.Text()
-     checkScanner()
-     line_arr := strings.Split(line, ",")
-     p, err := strconv.ParseFloat(line_arr[open_index], 64) 
-     if err != nil {
-          log.Fatal(err)
-     }
-     // layout := "2006-01-02T15:04:05.000Z"
-     layout := "2006-01-02 15:04:05"
-     t, err := time.Parse(layout, line_arr[1])
-     if err != nil {
-          log.Fatal(err)
-     }
-     fmt.Println(t)
+     p := readPrice()
+     scanner.Scan()
      return p
+     // line := scanner.Text()
+     // checkScanner()
+     // line_arr := strings.Split(line, ",")
+     // p, err := strconv.ParseFloat(line_arr[open_index], 64) 
+     // if err != nil {
+     //      log.Fatal(err)
+     // }
+     // // layout := "2006-01-02T15:04:05.000Z"
+     // layout := "2006-01-02 15:04:05"
+     // t, err := time.Parse(layout, line_arr[date_time_index])
+     // if err != nil {
+     //      log.Fatal(err)
+     // }
+     // fmt.Println(t)
+     // return p
 }
 
 func CurrentPrice() float64 {
