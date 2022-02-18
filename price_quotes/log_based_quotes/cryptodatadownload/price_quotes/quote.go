e86eeb2ed1afd99e35b25dac26e8ffaf8691871b
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-18 12:45:18 -0800

    coding

--- price_quotes/log_based_quotes/cryptodatadownload/price_quotes/quote.go
+++ price_quotes/log_based_quotes/cryptodatadownload/price_quotes/quote.go
@@ -14,11 +14,13 @@ const datafile = "/Users/Ke015t7/.gvm/pkgsets/go1.17.7/global/src/github.com/khu
 const open_index = 3
 const date_time_index = 1
 const date_time_layout = "2006-01-02 15:04:05"
-const start_time = time.Parse(date_time_layout, "2020-11-21 07:21:00")
-const end_time = time.Parse(date_time_layout, "2020-12-21 07:21:00")
+const start_time_str = time.Parse(date_time_layout, "2020-11-21 07:21:00")
+const end_time_str = time.Parse(date_time_layout, "2020-12-21 07:21:00")
 
 var file *os.File = nil
 var scanner *bufio.Scanner = nil
+var start_time time.Time
+var end_time time.Time
 
 func Init() {
      fmt.Println("cryptodatadownload price_quotes Init()")
@@ -27,6 +29,17 @@ func Init() {
           log.Fatal(err)
      }
      file = f
+
+     start_time, err = time.Parse(date_time_layout, start_time_str) 
+     if err != nil {
+          log.Fatal(err)
+     }
+
+     end_time, err = time.Parse(date_time_layout, end_time_str) 
+     if err != nil {
+          log.Fatal(err)
+     }
+     
      scanner = bufio.NewScanner(file)
      checkScanner()
 }
@@ -41,6 +54,7 @@ func scanToStartDate() {
 
      for readDate().Before(start_time) {
          scanner.Scan() 
+         checkScanner()
      }
 }
 
