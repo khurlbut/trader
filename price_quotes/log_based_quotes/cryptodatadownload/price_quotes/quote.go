cf9dedc927be2a8ce511d1e3e4885f7a46607b9e
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-18 10:36:20 -0800

    coding

--- price_quotes/log_based_quotes/cryptodatadownload/price_quotes/quote.go
+++ price_quotes/log_based_quotes/cryptodatadownload/price_quotes/quote.go
@@ -6,9 +6,6 @@ import (
      "log"
 )
 
-// var spotPriceIndex = 0
-// var prices = []float64{10.00, 10.200, 10.404, 10.19592, 9.98784, 10.00}
-
 const datafile = "/Users/Ke015t7/.gvm/pkgsets/go1.17.7/global/src/github.com/khurlbut/trader/data/cryptodatadownload/Binance_BTCUSDT_minute.csv"
 
 var file *os.File = nil
@@ -27,19 +24,18 @@ func Close() {
      file.Close()
 }
 
+var i = 0
 func HasNextPrice() bool {
+     if i++ < 10 {
+          return true
+     }
      return false
-     // return spotPriceIndex < len(prices)
 }
 
 func NextPrice() float64 {
      return 0
-     // p := prices[spotPriceIndex]
-     // spotPriceIndex++
-     // return p
 }
 
 func CurrentPrice() float64 {
      return 0.0
-     // return prices[spotPriceIndex]
 }
