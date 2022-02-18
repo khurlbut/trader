9eefd67c2c416aadd624275179dc49bd5f4c4ad5
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-18 09:37:51 -0800

    coding

--- prototype/prototype.go
+++ prototype/prototype.go
@@ -3,7 +3,6 @@ package prototype
 import
 (
      "fmt"
-     // "github.com/khurlbut/trader/price_quotes"
      "github.com/khurlbut/trader/price_quotes/log_based_quotes/cryptodatadownload/price_quotes"
 )
 
