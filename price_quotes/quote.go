3c550f59d8d430eb21eed9fc70c8d2c5a28b45ac
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-19 15:19:24 -0800

    coding

--- price_quotes/quote.go
+++ price_quotes/quote.go
@@ -12,7 +12,8 @@ func Close() {
 }
 
 // var prices = []float64{5.0, 8.0, 4.0, 2.0, 6.0, 7.0}
-var prices = []float64{1000.0, 1020.0, 1040.4, 1019.592, 998.784, 1000}
+// var prices = []float64{1000.0, 1020.0, 1040.4, 1019.592, 998.784, 1000}
+var prices = []float64{5000.0, 7500.00}
 
 func HasNextPrice() bool {
      return spotPriceIndex < len(prices)
