51762298e87add72cdc1032b332bf62704116bd2
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-18 19:36:47 -0800

    coding

--- prototype/prototype.go
+++ prototype/prototype.go
@@ -34,7 +34,7 @@ func PricingLoop() string {
      
      purse.Init((0.5*initalFiatAmount)/spotPrice, 0.5*initalFiatAmount, purseFiatTargetPercent)
 
-     fmt.Printf("Initial: %s\n", purse.String(spotPrice))
+     fmt.Printf("Start: %s\n", purse.String(spotPrice))
 
      for price_quotes.HasNextPrice() {
           spotPrice = price_quotes.NextPrice()
