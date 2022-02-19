a0b0d04379ada5f51fb725ef533685ae462ea177
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-19 14:39:01 -0800

    coding

--- purse/purse.go
+++ purse/purse.go
@@ -36,13 +36,13 @@ func ValueAt(spot float64) float64 {
 
 func CashRequiredToAlignWithTarget(spot float64) float64 {
      target := ValueAt(spot) * targetCashPercentage
-     fmt.Printf("CashRequiredToAlignWithTarget: %f\n", target)
+     // fmt.Printf("CashRequiredToAlignWithTarget: %f\n", target)
      holdings := CashHoldings() 
      adjustment := target - holdings
      fee := tradingFee(adjustment)
      if feeCausesCostOverrun(fee, adjustment, holdings) {
-          fmt.Printf("returning adjustment + fee: %f\n", adjustment + fee)
-     os.Exit(0)
+     //      fmt.Printf("returning adjustment + fee: %f\n", adjustment + fee)
+     // os.Exit(0)
           return adjustment + fee
      }
      os.Exit(0)
@@ -50,7 +50,7 @@ func CashRequiredToAlignWithTarget(spot float64) float64 {
 }
 
 func feeCausesCostOverrun(fee float64, adjustment float64, holdings float64) bool {
-     fmt.Printf("fee: %f, adjustment: %f, holdings: %f\n", fee, adjustment, holdings)
+     // fmt.Printf("fee: %f, adjustment: %f, holdings: %f\n", fee, adjustment, holdings)
      return (fee + math.Abs(adjustment)) > holdings 
 }
 
