c6f2c3488f61c50d2939f09b2b55148703139c2d
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-20 13:06:59 -0800

    coding

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import(
 
 func main() {
 	p := purse.NewPurse(123.456, 789.123)
-	fmt.Printf("p.NewString: %" + p.NewString())
+	fmt.Printf("p.NewString: %" + p.Properties())
 	fmt.Println(prototype.PricingLoop())
 }
