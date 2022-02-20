3795b069a7bea253a8c627bd7b4c5aa0099242c0
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-20 12:48:55 -0800

    coding

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import(
 	"fmt"
 	"github.com/khurlbut/trader/prototype"
+	"github.com/khurlbut/trader/purse"
 )
 
 func main() {
+	var purse *Purse
+	purse = NewPurse(123.456, 789.123)
 	fmt.Println(prototype.PricingLoop())
 }
