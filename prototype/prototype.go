75e1bd7351b5b9fcdfafaf7794e6d7d6bf1b1fdb
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-20 09:07:36 -0800

    coding

--- prototype/prototype.go
+++ prototype/prototype.go
@@ -15,11 +15,11 @@ import
 
 const BuyTrigger = 0.02
 const SellTrigger = 0.02
-const targetCashPercentage = 0.2
+const targetCashPercentage = 0.5
 
 const tradingFeePercentage = 0.006
 
-var initalCashAmount = 10000.00
+var initalCashAmount = 16000.00
 
 func PricingLoop() string {
      price_quotes.Init()
