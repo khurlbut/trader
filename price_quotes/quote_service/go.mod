d3a9b38aeabfe4b7e4c68351a625875b5eae7da2
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-21 15:48:43 -0800

    coding

---
+++ price_quotes/quote_service/go.mod
@@ -0,0 +1,3 @@
+module github.com/khurlbut/trader/price_quotes/quote_service
+
+go 1.17
---
+++ price_quotes/quote_service/quote.go
@@ -0,0 +1,13 @@
+package quote_service
+
+import (
+	"testing";
+)
+
+func TestSayHello(t *testing.T) {
+	actual := BuildHello()
+	expected := "Hello, world."
+    if expected != actual {
+		t.Errorf("Error occured while testing sayhello: '%s' != '%s'", expected, actual);
+    }
+}
