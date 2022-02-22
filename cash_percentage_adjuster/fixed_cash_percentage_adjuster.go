2ed43862666ca06ec7eff0f4b3a138258f550e7f
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-22 10:29:10 -0800

    coding

--- cash_percentage_adjuster/fixed_cash_percentage_adjuster.go
+++ cash_percentage_adjuster/fixed_cash_percentage_adjuster.go
@@ -12,7 +12,7 @@ type FixedCashPercentageTable struct {}
 func NewFixedCashAdjuster() *FixedCashPercentageTable {
 	log.Fatal("Not Implemented")
   // props := properties.MustLoadFile(propertiesFile, properties.UTF8)
-	// return nil
+	return nil
 }
 
 func (c *FixedCashPercentageTable) CashPercentageTarget(spot float64) float64 {
