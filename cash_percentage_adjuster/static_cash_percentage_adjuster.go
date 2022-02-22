f089d669b1d749c04f0cda2354582f31a99f7f61
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-22 10:23:45 -0800

    coding

---
+++ cash_percentage_adjuster/static_cash_percentage_adjuster.go
@@ -0,0 +1,20 @@
+package cash_percentage_adjuster
+
+import(
+	"log"
+  "github.com/magiconair/properties"
+)
+
+type FixedCashPercentageTable struct {}
+
+const propertiesFile = "/Users/Ke015t7/.gvm/pkgsets/go1.17.7/global/src/github.com/khurlbut/trader/campaign.properties"
+
+func NewFixedCashAdjuster() *FixedCashPercentageTable {
+  props := properties.MustLoadFile(propertiesFile, properties.UTF8)
+	return nil
+}
+
+func (c *SlidingCashPercentageTable) CashPercentageTarget(spot float64) float64 {
+	log.Fatal("Not Implemented")
+	return -1.0
+}
