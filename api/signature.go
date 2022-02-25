8daac09c5f402332a94221c47b90a7f0e8743e33
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-25 11:38:47 -0800

    coding

--- api/signature.go
+++ api/signature.go
@@ -6,9 +6,9 @@ import (
     "os/exec"
 )
 
-func Signature(timestamp string) {
+func Signature(timestamp string, secret_key string) {
 
-    out, err := exec.Command("echo", "-n", "timestamp="+timestamp).Output()
+    out, err := exec.Command("echo", "-n", "timestamp="+timestamp, "|", "openssl", "dgst", "-sha256", "-hmac", secret_key).Output()
 
     if err != nil {
         log.Fatal(err)
--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import(
 	"github.com/khurlbut/trader/api"
 	"github.com/khurlbut/trader/campaign"
 	"github.com/khurlbut/trader/prototype"
+	// "github.com/magiconair/properties"
 )
 
 func main() {
+	// props := properties.MustLoadFile("api/binance.properties", properties.UTF8)
 	// api.Ls()
 	ts := api.Timestamp()
 	fmt.Println(ts)
-	api.Signature(ts)
+	api.Signature(ts, "YT4DtosTWcptwdlScXUISNA9FQ5FxsXGtnSoROeTxyuJBQEGfi8X9lziav2bW9tf")
 	os.Exit(0)
 	c := campaign.NewCampaign()
 	fmt.Println(prototype.PricingLoop(c))
