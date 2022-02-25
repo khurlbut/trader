a56fc5af9321e190651f1d1729ccd5202a13b0e3
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-25 10:36:35 -0800

    coding

--- api/timestamp.go
+++ api/timestamp.go
@@ -2,19 +2,21 @@ package api
 
 import (
     "fmt"
-    "log"
-    "os/exec"
+    "time"
 )
 
-func Timestamp() {
+func Timestamp()  {
+    m := time.Now().UnixMilli()
+    fmt.Println("Type of m: %t", m)
 
-    out, err := exec.Command("date +v%s000").Output()
-    fmt.Printf("out = %T\n", out)
+    return 
+    // out, err := exec.Command("date", "+v%s000").Output()
+    // fmt.Printf("out = %T\n", out)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+    // if err != nil {
+    //     log.Fatal(err)
+    // }
 
-    fmt.Println(string(out))
+    // fmt.Println(string(out))
     // return fmt.Sprintln("%s000", out')
 }
