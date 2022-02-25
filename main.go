19d5a1736e04997d76e341d6b5c3cf21640a3d4c
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-25 14:08:19 -0800

    coding

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import(
 
 func main() {
 	props := properties.MustLoadFile("api/binance.properties", properties.UTF8)
-	api_key := props.GetString("api_key", "")
-	secret_key := props.GetString("secret_key", "")
+	api_key := props.GetString("api_key_prop", "")
+	secret_key := props.GetString("secret_key_prop", "")
 	fmt.Printf("api_key: %s\nsecret_key: %s\n", api_key, secret_key)
 	// api.Ls()
 	ts := api.Timestamp()
