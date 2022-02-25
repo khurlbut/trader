ca46a78493c020abb9b319de87fa7c52a5db3dc0
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-25 10:12:59 -0800

    coding

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import(
+	"os"
 	"fmt"
 	"github.com/khurlbut/trader/campaign"
 	"github.com/khurlbut/trader/prototype"
 )
 
 func main() {
+	os.Exit(0)
 	c := campaign.NewCampaign()
 	fmt.Println(prototype.PricingLoop(c))
 }
