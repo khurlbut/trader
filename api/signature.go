70eb4730bf47f0a0ec7f6aeb86b25e4178489df3
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-25 11:51:15 -0800

    coding

--- api/signature.go
+++ api/signature.go
@@ -4,15 +4,34 @@ import (
     "fmt"
     "log"
     "os/exec"
+    "crypto/hmac"
+    "crypto/sha256"
+    "encoding/hex"
 )
 
 func Signature(timestamp string, secret_key string) {
 
-    out, err := exec.Command("echo", "-n", "timestamp="+timestamp, "|", "openssl", "dgst", "-sha256", "-hmac", secret_key).Output()
+    // // out, err := exec.Command("echo", "-n", "timestamp="+timestamp, "|", "openssl", "dgst", "-sha256", "-hmac", secret_key).Output()
+    // out, err := exec.Command("`echo -n timestamp="+ timestamp +"| openssl dgst -sha256 -hmac "secret_key"`).Output()
 
-    if err != nil {
-        log.Fatal(err)
-    }
+    // if err != nil {
+    //     log.Fatal(err)
+    // }
 
-    fmt.Println(string(out))
+    // fmt.Println(string(out))
+
+    // secret := "mysecret"
+    // data := "data"
+    // fmt.Printf("Secret: %s Data: %s\n", secret, data)
+
+    // Create a new HMAC by defining the hash type and the key (as byte array)
+    h := hmac.New(sha256.New, []byte(secret_key))
+
+    // Write Data to it
+    h.Write([]byte(timestamp))
+
+    // Get result and encode as hexadecimal string
+    sha := hex.EncodeToString(h.Sum(nil))
+
+    fmt.Println("Result: " + sha)
 }
