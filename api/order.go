aec212ac032748985e5c31350751b589c539e12f
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-25 15:05:07 -0800

    coding

--- api/order.go
+++ api/order.go
@@ -8,16 +8,25 @@ import (
 )
 
 func Order(api_key string, timestamp string, signature string) {
-  url := fmt.Sprintf("https://api.binance.us/api/v3/account?timestamp=%s&signature=%s -H X-MBX-APIKEY: %s", timestamp, signature, api_key)
+  // url := fmt.Sprintf("https://api.binance.us/api/v3/account?timestamp=%s&signature=%s -H X-MBX-APIKEY: %s", timestamp, signature, api_key)
+  url := fmt.Sprintf("https://api.binance.us/api/v3/account?timestamp=%s&signature=%s", timestamp, signature)
   fmt.Printf("\n%s\n",url)
-  resp, err := http.Get(url)
+  
+  client := &http.Client{
+    CheckRedirect: redirectPolicyFunc,
+  }
+  req, err := httpNewRequest("GET", url nil)
+  req.Header.Add("X-MBX-APIKEY", api_key)
+
+  resp, err :=client.Do(req)
   if err != nil {
        log.Fatal(err)
   }
+
   defer resp.Body.Close()
   body, err := io.ReadAll(resp.Body)
   if err != nil {
        log.Fatal(err)
   }  
-  fmt.Println(body)
+  fmt.Println(string(body))
 }
