11531a759b2bc95493575f890b33d5b6c4733aed
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-25 14:05:56 -0800

    coding

--- api/order.go
+++ api/order.go
@@ -1,14 +1,15 @@
 package api
 
 import (
+    "io"
     "fmt"
-    "time"
-    "strconv"
+    "log"
+    "net/http"
 )
 
 func Order(api_key string, timestamp string, signature string) {
   url := fmt.Sprintf("https://api.binance.us/api/v3/account?timestamp=%s&signature=%s -H X-MBX-APIKEY: %s", timestamp, signature, api_key)
-  resp, err := http.Get(qs.priceEndPoint)
+  resp, err := http.Get(url)
   if err != nil {
        log.Fatal(err)
   }
