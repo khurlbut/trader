85759b5e10f1ff4587a7466019cb31f7a1718312
Kevin Hurlbut <kevin_hurlbut@gap.com>
2022-02-27 09:39:09 -0800

    coding

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	fmt.Printf("api_key: %s\nsecret_key: %s\n", api_key, secret_key)
 	// api.Ls()
 	ts := api.Timestamp()
-	api.Order(api_key, secret_key, ts))
+	api.Order(api_key, secret_key, ts)
 	// fmt.Println(ts)
 	os.Exit(0)
 	c := campaign.NewCampaign()
